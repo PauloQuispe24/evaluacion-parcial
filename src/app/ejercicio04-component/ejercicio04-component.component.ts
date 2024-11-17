896dcbe46a2020a69cc10c8690b20affa59093d4
Paulo Quispe <paulo.quispe.horna@gmail.com>
2024-11-17 04:19:19 -0500

    Ejercicio 04 resuelto

--- src/app/ejercicio04-component/ejercicio04-component.component.ts
+++ src/app/ejercicio04-component/ejercicio04-component.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-ejercicio04-component',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule
+  ],
   templateUrl: './ejercicio04-component.component.html',
   styleUrl: './ejercicio04-component.component.css'
 })
 export class Ejercicio04ComponentComponent {
+  mostrar: boolean = false;
+  resultados: Array<{numero: number, cuadrado: number, mitad: number}> = [];
+  displayedColumns: string[] = ['numero', 'cuadrado', 'mitad'];
 
+  calcularCuadradoYMitad() {
+    for (let i = 15; i <= 70; i++) {
+      this.resultados.push({
+        numero: i,
+        cuadrado: i * i,
+        mitad: i / 2
+      });
+    }
+
+    this.mostrar = true;
+  }
 }
