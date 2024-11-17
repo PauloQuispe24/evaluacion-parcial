2e8dd8273d91f2c2c77fec0fb7cb04720a7e807c
Paulo Quispe <paulo.quispe.horna@gmail.com>
2024-11-17 02:35:27 -0500

    Creando las rutas del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Ejercicio01ComponentComponent } from './ejercicio01-component/ejercicio01-component.component';
+import { Ejercicio02ComponentComponent } from './ejercicio02-component/ejercicio02-component.component';
+import { Ejercicio03ComponentComponent } from './ejercicio03-component/ejercicio03-component.component';
+import { Ejercicio04ComponentComponent } from './ejercicio04-component/ejercicio04-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "Ejercico01", component: Ejercicio01ComponentComponent },
+  { path: "Ejercico02", component: Ejercicio02ComponentComponent },
+  { path: "Ejercico03", component: Ejercicio03ComponentComponent },
+  { path: "Ejercico04", component: Ejercicio04ComponentComponent }
+];
